9e99e7277b3f6b96d884464d8ca59fe027fb27ef
Marcos Puglisi <profe.puglisi@gmail.com>
2021-11-08 21:15:58 -0300

    Revisar service char

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharsComponent } from './components/chars/chars.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'characters', component: CharsComponent},
+  {path: 'characters/:id', component: CharsComponent},
+  {path: '**', redirectTo: '/characters'}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CharsComponent } from './components/chars/chars.component';
+import { CharComponent } from './components/char/char.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { NavbarComponent } from './core/layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CharsComponent,
+    CharComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
